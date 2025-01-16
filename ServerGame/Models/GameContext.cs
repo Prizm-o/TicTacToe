7a5ebd2fc15e37df2085b26a47205449a0d8f5dd
Prizm-o <superwulf123@gmail.com>
2025-01-16 18:32:05 +0500

    First commit

---
+++ ServerGame/Models/GameContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ServerGame.Models
+{
+    public class GameContext : DbContext
+    {
+        public DbSet<GameResult> GameResults { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseNpgsql("Host=localhost;Database=TicTacToe;Username=postgres;Password=postgres");
+        }
+    }
+}
---
+++ ServerGame/Startup.cs
@@ -0,0 +1,38 @@
+﻿using ServerGame.Hubs;
+using ServerGame.Models;
+
+namespace ServerGame
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+            services.AddDbContext<GameContext>();
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllOrigins",
+                    builder => builder.AllowAnyOrigin()
+                                      .AllowAnyMethod()
+                                      .AllowAnyHeader());
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+
+            app.UseCors("AllowAllOrigins");
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHub<GameHub>("/gamehub");
+            });
+        }
+    }
+}
